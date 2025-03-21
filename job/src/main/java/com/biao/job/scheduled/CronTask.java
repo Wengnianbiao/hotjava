6a5f0d914057dddb44b2b9cecb0dda582f2fe763
nianbiao <1192317022@qq.com>
2025-03-21 11:04:52 +0800

    docs:@Scheduled优缺点和使用场景

--- job/src/main/java/com/biao/job/scheduled/CronTask.java
+++ job/src/main/java/com/biao/job/scheduled/CronTask.java
@@ -6,6 +6,17 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * @Scheduled是Spring框架集成的定时任务能力，使用简单方便，因此适合单体服务场景。但使用过可以知道，存在一下瓶颈：
+ *
+ * 1、无法适配分布式环境下，即使在定时任务中业务逻辑中加分布式锁，但对代码侵袭性太强；
+ *
+ * 2、无法动态修改定时任务的执行规则，需要重新发布服务上线；
+ *
+ * 3、需要开发手动编写日志来获取定时任务的元数据信息，譬如执行时间这种功能。
+ *
+ * 4、其他一些高可用的能力，分片、告警等；
+ */
 @Component
 public class CronTask {
 
